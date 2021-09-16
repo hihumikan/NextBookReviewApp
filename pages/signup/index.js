bfc04fe073470944da42fcd10dd23be5d6ba13ac
hihumikan <musecharo@gmail.com>
2021-09-16 22:58:11 +0900

    ini

---
+++ pages/signup/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "next/link";
+
+function Signup() {
+  return (
+    <>
+      <h1>Signup</h1>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+    </>
+  );
+}
+
+export default Signup;
